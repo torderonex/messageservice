1e5393b36d30287ee2fae00420bd4a0938d4b8d9
torderonex <eligorko.info@yandex.ru>
2024-07-24 12:25:06 +0300

    add: config

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,8 +1,22 @@
 package main
 
+import (
+	"fmt"
+	"github.com/joho/godotenv"
+	"github.com/torderonex/messageservice/internal/config"
+	"log"
+)
+
+func init() {
+	if err := godotenv.Load("config/.env"); err != nil {
+		log.Fatalf("Error loading .env file: %v", err)
+	}
+}
+
 func main() {
 	//init cfg
-
+	cfg := config.MustLoad()
+	fmt.Println(cfg)
 	//init logger
 
 	//init repo
